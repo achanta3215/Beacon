f1b33361ffcc2205f6f5f62d0d5a8cc076ed2272
Krishna Sameer Achanta <Krishna.Achanta@maximintegrated.com>
2017-11-01 20:34:58 +0530

    initial commit

---
+++ app/src/main/java/com/example/krishnaachanta/beacon/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.krishnaachanta.beacon
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import org.altbeacon.beacon.Beacon
+import org.altbeacon.beacon.BeaconParser
+import org.altbeacon.beacon.BeaconTransmitter
+import java.util.*
+import java.util.Arrays.asList
+import kotlin.collections.ArrayList
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val beacon = Beacon.Builder()
+
+                .setId1("2f234454-cf6d-4a0f-adf2-f4911ba9ffa6")
+                .setId2("1")
+                .setId3("2")
+                .setManufacturer(0x0118)
+                .setTxPower(-59)
+                .setDataFields(Arrays.asList(0L))
+                .build()
+        val beaconParser = BeaconParser()
+                .setBeaconLayout("m:2-3=beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25")
+        val beaconTransmitter = BeaconTransmitter(applicationContext, beaconParser)
+        beaconTransmitter.startAdvertising(beacon)
+    }
+}
